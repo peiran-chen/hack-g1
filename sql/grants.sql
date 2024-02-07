c07f4e7050da9b1bdc0728e70e718505f1b59275
Peiran Chen <peiran.chen@mq.edu.au>
2024-02-07 14:09:32 +1100

    add grants to grants.sql

--- sql/grants.sql
+++ sql/grants.sql
@@ -1,2 +1,6 @@
 -- grants
 grant all on all tables in schema hackathon.group_1 to role g1_role;
+
+grant usage on database hackathon to role g1_role;
+grant usage on schema hackathon.group_1 to role g1_role;
+grant usage on streamlit hackathon.group_1.estiunienrol to role g1_role;
